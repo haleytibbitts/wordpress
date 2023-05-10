58f3e9ac293f571914998133ddb04c5e27edf0ac
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2023-05-10 15:12:01 -0400

    creating block patterns

---
+++ Creating-Block-Patterns/lil-block-patterns/block-patterns.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name:     Block Patterns
+ * Plugin URI:      https://linkedin.com/learning/
+ * Description:     A collection of block patterns
+ * Version:         1.0.0
+ * Author:          Joe Casabona
+ * Author URI:     	https:/casabona.org/
+ * License:         GPL-3.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+ 
+ // If this file is called directly, abort.
+  if ( ! defined( 'WPINC' ) ) {
+	  die;
+  }
+  
+  /**
+   * Currently plugin version.
+   */
+  define( 'LIL_BP_VERSION', '1.0.0' );
+  
+  /**
+   * Plugin URL
+   */
+  define( 'LIL_BP_URL', plugin_dir_url( __FILE__ ) ); //This include the trailing slash! 
+
+//   ENQUEUE STYLES
+  function lil_bp_add_styles() {
+    wp_enqueue_styles('lil-bp-styles', LIL_BP_URL . 'inc/lil-bp-styles.css');
+  };
+
+add_action('wp_enqueue_scripts', 'lil-bp-styles');
+
+// GRAB PATTERNS
+require_once('inc/patterns.php');
+  
